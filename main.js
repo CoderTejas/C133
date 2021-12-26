709735597d5f0ebef84a039a551c7adca65bdc42
CoderTejas <75924121+CoderTejas@users.noreply.github.com>
2021-12-26 00:30:03 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ img = "";
 status = "";
 objects = [];
 
-function preload()
-{
+function preload() {
     img = loadImage('dog_cat.jpg');
 }
 
-function setup()
-{
+function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
 
@@ -16,39 +14,32 @@ function setup()
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
-function modelLoaded()
-{
+function modelLoaded() {
     console.log("Cocossd Model Loaded");
     status = true;
     objectDetector.detect(img, gotResult);
 }
 
-function gotResult(error, results)
-{
-    if(error)
-    {
+function gotResult(error, results) {
+    if (error) {
         console.error(error);
     }
-    else
-    {
+    else {
         console.log(results);
         objects = results;
     }
 }
 
-function draw()
-{
+function draw() {
     image(img, 0, 0, 640, 420);
-    if(status != "")
-    {
-        for(i=0; i<objects.length; i++)
-        {
-            document.getElementById("status").innerHTML = "Status : Objects Detected";
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
             fill("#05f5b1");
             percent = floor(objects[i].confidence * 100);
-            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
-            stroke("#29f505");
+            stroke("#05f5b1");
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
